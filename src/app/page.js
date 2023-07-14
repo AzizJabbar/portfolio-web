717a0ec559e9e854fadb9104d2ba55593905c4e5
AzizJabbar <azizjabbar1412@gmail.com>
2023-07-14 16:35:25 +0700

    add video background

--- src/app/page.js
+++ src/app/page.js
@@ -12,6 +12,7 @@ import Fade from "react-reveal/Fade";
 import projects from "../data/projects.json";
 import careers from "../data/careers.json";
 import skills from "../data/skills.json";
+import VideoBackground from "@/components/VideoBackground";
 
 const navigation = [
   { name: "About", href: "#about-me" },
@@ -115,17 +116,18 @@ export default function Home() {
       </header>
 
       <div className="relative isolate px-6 pt-14 lg:px-8">
+        <VideoBackground />
         <div
           className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-xl sm:-top-80"
           aria-hidden="true"
         >
-          <div
+          {/* <div
             className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
                 "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
             }}
-          />
+          /> */}
         </div>
         <div className="ml-8 md:ml-16 lg:ml-24 max-w-2xl py-16 sm:py-32 lg:py-24">
           <div className="text-left">
@@ -293,13 +295,13 @@ export default function Home() {
           className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
           aria-hidden="true"
         >
-          <div
+          {/* <div
             className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
                 "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
             }}
-          />
+          /> */}
         </div>
       </div>
     </div>
