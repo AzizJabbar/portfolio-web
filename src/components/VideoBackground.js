14501bfd4d00ca920594f65a1f0f40735032f223
AzizJabbar <azizjabbar1412@gmail.com>
2023-07-14 16:53:38 +0700

    add videobackground (deleted unintentioinally)

---
+++ src/components/VideoBackground.js
@@ -0,0 +1,9 @@
+export default function VideoBackground() {
+  return (
+    <div className="absolute top-0 left-0 right-0 w-full h-full -z-10 blur-3xl overflow-hidden">
+      <video className="object-fill w-full h-full opacity-75" autoPlay loop muted>
+        <source src="/video/cut-slowed-boomerang.mp4" type="video/mp4" />
+      </video>
+    </div>
+  );
+}
